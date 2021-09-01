175801f08b502aa7ae9cb2407fe75ab2f8e9b91a
Akanksha Bharti <akankshabharti2104@gmail.com>
2021-09-01 09:27:38 +0530

    first commit

---
+++ department-service/src/main/java/com/extramarks/departmentservice/Controller.java
@@ -0,0 +1,20 @@
+package com.extramarks.departmentservice;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class Controller {
+
+    @Value("${app.service-name}")
+    private String serviceName;
+
+    @GetMapping("/service")
+    public String getServiceName() {
+        return "service name-> [" + this.serviceName + "]";
+    }
+
+}
